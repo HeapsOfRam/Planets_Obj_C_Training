dfc34b670c80448e0ad2e6e378c561845756500f
Ryan March <rmarch03@villanova.edu>
2014-11-17 09:50:31 -0500

    Initial Commit

---
+++ PlanetsProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PlanetsProject
+//
+//  Created by Ryan March on 11/17/14.
+//  Copyright (c) 2014 Ryan March. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
